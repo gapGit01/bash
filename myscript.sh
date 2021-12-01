59b9305e06d570083f6ed920b0c85d64ca24021e
Pasindu Lakmal <gamaara2020@gmail.com>
2021-12-02 00:04:29 +0530

    my fist bash test

---
+++ myscript.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/dash
+
+# ECHO COMMAND
+# echo Hello world
+
+# VARIABLES
+# Uppercase by convention
+NAME="pasindu"
+# echo "My name is $NAME"
+
+# USER INPUT
+# read -p "Enter your name: " NAMA
+#echo "Hello $NAMA, nice to meet you!"
+
+# SIMPLE IF STATEMENT
+if [ $NAME == "pasindu" ]
+then
+ echo "Your name is Pasindu"
+fi
